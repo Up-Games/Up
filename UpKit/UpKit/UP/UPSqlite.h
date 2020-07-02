432a6471113d2bf0af1ea3df73ce4a39a4e05d76
Ken Kocienda <kocienda@mac.com>
2020-07-02 08:53:00 -0700

    Code cleanup.

--- UpKit/UpKit/UP/UPSqlite.h
+++ UpKit/UpKit/UP/UPSqlite.h
@@ -11,7 +11,6 @@ struct sqlite3_stmt;
 namespace UP {
 
 sqlite3 *db_handle_acquire(const char *path, int(^db_create)(sqlite3 *));
-void db_close(sqlite3 *);
 sqlite3_stmt *db_statement(sqlite3 *, const char *sql);
 void db_begin_transaction(sqlite3 *);
 void db_commit_transaction(sqlite3 *);
--- UpKit/UpKit/UP/UPSqlite.mm
+++ UpKit/UpKit/UP/UPSqlite.mm
@@ -17,40 +17,16 @@ namespace UP {
 using db_handle_map = std::map<const char *, sqlite3 *>;
 using db_statement_map = std::map<const char *, sqlite3_stmt *>;
 
-db_handle_map &db_handles()
+static db_handle_map &db_handles()
 {
     static db_handle_map map;
     return map;
 }
 
-db_statement_map &db_prepared_statements()
+static db_statement_map &db_prepared_statements()
 {
     static db_statement_map map;
     return map;
-}
-
-sqlite3_stmt *db_statement(sqlite3 *db, const char *sql)
-{
-    if (db == nullptr) {
-        return nullptr;
-    }
-
-    sqlite3_stmt *stmt = nullptr;
-    db_statement_map &map = db_prepared_statements();
-    auto it = map.find(sql);
-    if (it != map.end()) {
-        stmt = it->second;
-    }
-    else {
-        int rc = sqlite3_prepare_v2(db, sql, (int)strlen(sql), &stmt, nullptr);
-        if (rc == SQLITE_OK) {
-            map.emplace(sql, stmt);
-        }
-        else {
-            LOG(DB, "*** error preparing statement: %d", rc);
-        }
-    }
-    return stmt;
 }
 
 sqlite3 *db_handle_acquire(const char *path, int(^db_create)(sqlite3 *))
@@ -86,33 +62,28 @@ sqlite3 *db_handle_acquire(const char *path, int(^db_create)(sqlite3 *))
     return db;
 }
 
-void db_close(sqlite3 *db)
+sqlite3_stmt *db_statement(sqlite3 *db, const char *sql)
 {
     if (db == nullptr) {
-        return;
+        return nullptr;
     }
     
-    db_statement_map &stmts_map = db_prepared_statements();
-    for (auto it : stmts_map) {
-        sqlite3_finalize(it.second);
+    sqlite3_stmt *stmt = nullptr;
+    db_statement_map &map = db_prepared_statements();
+    auto it = map.find(sql);
+    if (it != map.end()) {
+        stmt = it->second;
     }
-    stmts_map.clear();
-
-    db_handle_map &handles_map = db_handles();
-    for (auto it = handles_map.begin(); it != handles_map.end();) {
-        if (it->second == db) {
-            handles_map.erase(it);
-            break;
+    else {
+        int rc = sqlite3_prepare_v2(db, sql, (int)strlen(sql), &stmt, nullptr);
+        if (rc == SQLITE_OK) {
+            map.emplace(sql, stmt);
         }
         else {
-            ++it;
+            LOG(DB, "*** error preparing statement: %d", rc);
         }
     }
-
-    int rc = sqlite3_close(db);
-    if (rc != SQLITE_OK) {
-        LOG(DB, "*** error closing database: %d", rc);
-    }
+    return stmt;
 }
 
 void db_begin_transaction(sqlite3 *db)
@@ -137,6 +108,35 @@ void db_rollback_transaction(sqlite3 *db)
         return;
     }
     db_exec(db, sqlite3_exec(db, "ROLLBACK TRANSACTION;", nullptr, nullptr, nullptr));
+}
+
+void db_close(sqlite3 *db)
+{
+    if (db == nullptr) {
+        return;
+    }
+    
+    db_statement_map &stmts_map = db_prepared_statements();
+    for (auto it : stmts_map) {
+        sqlite3_finalize(it.second);
+    }
+    stmts_map.clear();
+    
+    db_handle_map &handles_map = db_handles();
+    for (auto it = handles_map.begin(); it != handles_map.end();) {
+        if (it->second == db) {
+            handles_map.erase(it);
+            break;
+        }
+        else {
+            ++it;
+        }
+    }
+    
+    int rc = sqlite3_close(db);
+    if (rc != SQLITE_OK) {
+        LOG(DB, "*** error closing database: %d", rc);
+    }
 }
 
 }  // namespace UP
