2007b7181249e2e2317b16d63a2283ff2da20ecc
Ken Kocienda <kocienda@mac.com>
2020-08-11 14:25:10 -0700

    Progress on UPDialogShare.

--- UPSpell/UPSpell/Code/UPDialogShare.mm
+++ UPSpell/UPSpell/Code/UPDialogShare.mm
@@ -19,6 +19,7 @@
 #import "UPTextPaths.h"
 
 using UP::SpellLayout;
+using Role = SpellLayout::Role;
 
 @interface UPDialogShare ()
 @property (nonatomic, readwrite) UPVectorLogoView *vectorLogoView;
@@ -50,43 +51,43 @@ using UP::SpellLayout;
     self.vectorLogoView = [UPVectorLogoView vectorLogoView];
     [self addSubview:self.vectorLogoView];
     self.vectorLogoView.frame = CGRectMake(0, 0, 160, 160);
-    self.vectorLogoView.center = layout.center_for(SpellLayout::Role::ChallengeInterstitialLogo);
+    self.vectorLogoView.center = layout.center_for(Role::ChallengeInterstitialLogo);
 
     self.wordMarkLabel = [UPLabel label];
     self.wordMarkLabel.string = @"UP SPELL";
     self.wordMarkLabel.font = layout.word_mark_font();
     self.wordMarkLabel.textAlignment = NSTextAlignmentCenter;
-    self.wordMarkLabel.frame = layout.frame_for(SpellLayout::Role::ChallengeInterstitialWordMark);
+    self.wordMarkLabel.frame = layout.frame_for(Role::ChallengeInterstitialWordMark);
     [self addSubview:self.wordMarkLabel];
     
     self.shareLabel = [UPLabel label];
     self.shareLabel.string = @"CHALLENGE!";
     self.shareLabel.font = layout.share_prompt_font();
     self.shareLabel.textAlignment = NSTextAlignmentCenter;
-    self.shareLabel.frame = layout.frame_for(SpellLayout::Role::DialogMessageSharePrompt);
+    self.shareLabel.frame = layout.frame_for(Role::DialogMessageSharePrompt);
     [self addSubview:self.shareLabel];
 
     self.scoreToTopLabel = [UPLabel label];
     self.scoreToTopLabel.font = layout.share_score_to_beat_font();
     self.scoreToTopLabel.textAlignment = NSTextAlignmentCenter;
-    self.scoreToTopLabel.frame = layout.frame_for(SpellLayout::Role::DialogMessageShareScoreToBeat);
+    self.scoreToTopLabel.frame = layout.frame_for(Role::DialogMessageShareScoreToBeat);
     [self addSubview:self.scoreToTopLabel];
 
     self.cancelButton = [UPTextButton textButton];
     self.cancelButton.labelString = @"CANCEL";
     [self.cancelButton setLabelColorCategory:UPColorCategoryContent forState:UPControlStateNormal];
-    self.cancelButton.frame = layout.frame_for(SpellLayout::Role::DialogButtonAlternativeResponse);
+    self.cancelButton.frame = layout.frame_for(Role::DialogButtonAlternativeResponse);
     [self addSubview:self.cancelButton];
 
     self.goButton = [UPTextButton textButton];
     self.goButton.labelString = @"PLAY";
     [self.goButton setLabelColorCategory:UPColorCategoryContent forState:UPControlStateNormal];
-    self.goButton.frame = layout.frame_for(SpellLayout::Role::DialogButtonDefaultResponse);
+    self.goButton.frame = layout.frame_for(Role::DialogButtonDefaultResponse);
     [self addSubview:self.goButton];
 
     self.helpButton = [UPButton roundHelpButton];
     [self.helpButton setLabelColorCategory:UPColorCategoryContent forState:UPControlStateNormal];
-    self.helpButton.frame = layout.frame_for(SpellLayout::Role::DialogHelpButton);
+    self.helpButton.frame = layout.frame_for(Role::DialogHelpButton);
     [self addSubview:self.helpButton];
 
     [self updateThemeColors];
