a0331418bc479eb2f00a0243ba53b8552aa0ab8f
Ken Kocienda <kocienda@mac.com>
2020-09-08 11:29:21 -0700

    Back off Whoops volume a tad.

--- UPSpell/UPSpell/Code/UPSpellGameController.mm
+++ UPSpell/UPSpell/Code/UPSpellGameController.mm
@@ -3140,7 +3140,7 @@ static NSString * const UPSpellInProgressGameFileName = @"up-spell-in-progress-g
     [soundPlayer setFilePath:[bundle pathForResource:@"Happy-4" ofType:@"aac"] forSoundID:UPSoundIDHappy4 volume:0.9f playerCount:3];
     [soundPlayer setFilePath:[bundle pathForResource:@"Sad-1" ofType:@"aac"] forSoundID:UPSoundIDSad1 volume:0.7f playerCount:2];
     [soundPlayer setFilePath:[bundle pathForResource:@"Sad-2" ofType:@"aac"] forSoundID:UPSoundIDSad2 volume:0.65f playerCount:2];
-    [soundPlayer setFilePath:[bundle pathForResource:@"Whoops" ofType:@"aac"] forSoundID:UPSoundIDWhoops volume:0.75f playerCount:3];
+    [soundPlayer setFilePath:[bundle pathForResource:@"Whoops" ofType:@"aac"] forSoundID:UPSoundIDWhoops volume:0.7f playerCount:3];
 }
 
 - (void)updateSoundAndTunesSettings
