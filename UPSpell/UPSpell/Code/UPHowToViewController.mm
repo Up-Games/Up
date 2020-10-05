a89e967cfdea0fb4c3cb3d30963baeef9d12c485
Ken Kocienda <kocienda@mac.com>
2020-10-05 08:45:02 -0700

    App Preview capture.

--- UPSpell/UPSpell/Code/UPHowToViewController.mm
+++ UPSpell/UPSpell/Code/UPHowToViewController.mm
@@ -28,7 +28,7 @@
     [self.pane commonConfigure];
     [self.pane configureForAppPreviewCapture];
 
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [self.pane startTutorial];
     });
 }
